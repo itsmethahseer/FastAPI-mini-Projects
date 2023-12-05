fa076f71fdec423eaeaa412472733bc41adaa4f3
Muhammed Thahseer <zacthahseer123@gmail.com>
2023-12-05 17:13:34 +0530

    updated the mysql models.py file

--- mysql_db_structure_fetch/models.py
+++ mysql_db_structure_fetch/models.py
@@ -3,13 +3,10 @@
 import mysql.connector
 import pandas as pd
 import json
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="admin",
-    database="mysql"
-)
 
+connection = mysql.connector.connect(
+    host="localhost", user="root", password="admin", database="admin"
+)
 
 
 # Fetch tables
@@ -24,80 +21,35 @@ for table in tables:
     table_name = table[0]
     cursor.execute(f"DESCRIBE {table_name};")
     columns = cursor.fetchall()
-    table_structures[table_name] = [column[0] for column in columns]
+    table_structures[table_name] = [
+        {"name": column[0], "Type": column[1]} for column in columns
+    ]
 
-json_format = json.dumps(table_structures,indent=2)
+json_format = json.dumps(table_structures, indent=2)
 print(json_format)
 
 # getting table schema as json format
 
 
-
-
 # getting table records as json format
-# sql_query = 'SELECT * FROM hello2'
-# # read the data using pandas
-# df = pd.read_sql(sql_query,connection)
+sql_query = "SELECT * FROM hello2"
+# read the data using pandas
+df = pd.read_sql(sql_query, connection)
 
-# # convert this dataframe into json format
+# convert this dataframe into json format
 
-# json_data = df.to_json(orient="records")
-# print(json_data)
-
-
+json_data = df.to_json(orient="records")
+print(json_data)
 
 
 # geting table schema as json format
 
-# cursor = connection.cursor(dictionary=True)
-# sql_query = "DESCRIBE hello2"
+cursor = connection.cursor(dictionary=True)
+sql_query = "DESCRIBE hello2"
 
-# # Execute SQL Query and Fetch Schema
-# cursor.execute(sql_query)
-# table_schema = cursor.fetchall()
+# Execute SQL Query and Fetch Schema
+cursor.execute(sql_query)
+table_schema = cursor.fetchall()
 
-# schema_json = json.dumps(table_schema, indent=2)
-# print(schema_json)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+schema_json = json.dumps(table_schema, indent=2)
+print(schema_json)
